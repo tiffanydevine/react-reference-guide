de04f139de50f48e69a7d431ca519d11428b6082
Tiffany <sunngreen@gmail.com>
2016-11-09 22:49:28 -0800

    link and title fields added

--- src/components/Weeks/input.js
+++ src/components/Weeks/input.js
@@ -4,6 +4,8 @@ export default class Input extends React.Component {
 
 addLink(event) {
 		event.preventDefault(); 
+		let title = this.refs.title.value;
+		let link =  this.refs.title.value;
         console.log(this.refs.link.value)
     }
 
@@ -11,8 +13,14 @@ render(){
 
 	return(
 		<form onSubmit={this.addLink.bind(this)}>
-			<input type='text' ref='link' />
-			<button type='submit'>Submit</button>
+			<p>
+				Link Title: <input type='text' ref='link' />
+				<button type='submit'>Submit</button>
+			</p>
+			<p>
+				Link: <input type='text' ref='link' />
+				<button type='submit'>Submit</button>
+			</p>
 		</form>
 
 		)
